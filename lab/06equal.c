916bb3bb6fd767defae53b27d4beba7986cc51aa
princeguptadev <princegupta180308@gmail.com>
2025-09-24 16:54:39 +0530

    lab 06equal.c

---
+++ lab/06equal.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int firstNum = 12, secondNum = 12;
+
+    if(firstNum == secondNum) {
+        printf("Equal");
+    }
+    else {
+        printf("Not Equal");
+    }
+
+    return 0;
+}
